c0d850a286a7f7bd9ce25e44f8785b7be18ef4d4
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2023-02-16 15:42:48 -0300

    header creation and rm button cart

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logoImg from "../../../src/assets/logo.svg";
+import Image from "next/image";
+import { HeaderContainer } from "./styles"
+import Link from 'next/link';
+import Cart from '../Cart';
+import { useRouter } from 'next/router';
+
+export function Header() {
+
+  const { pathname } = useRouter()
+
+  const showCartButton = pathname !== '/success'
+  return (
+    <HeaderContainer>
+      <Link href='/'>
+        <Image src={logoImg}
+          alt="logo"
+          width={129}
+          height={52}
+        />
+      </Link>
+      {showCartButton && <Cart />}
+
+
+    </HeaderContainer>
+  )
+}
